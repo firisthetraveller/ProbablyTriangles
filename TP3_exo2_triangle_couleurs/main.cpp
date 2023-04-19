800e7ab3008d526bc2d247d7e988a72cd5535a65
gluong <16323642+guy-luong@users.noreply.github.com>
2023-04-19 18:06:38 +0200

    Add: Moons

--- TP3_exo2_triangle_couleurs/main.cpp
+++ TP3_exo2_triangle_couleurs/main.cpp
@@ -1,21 +1,107 @@
 #include "p6/p6.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/constants.hpp>
 
-int main()
-{
-    auto ctx = p6::Context{{1280, 720, "TP3 EX2"}};
-    ctx.maximize_window();
+int main() {
+  auto ctx = p6::Context{{1280, 720, "TP3 EX2"}};
+  ctx.maximize_window();
 
+  struct Vertex2DColor {
+    glm::vec2 _position;
+    glm::vec3 _color;
+
+    Vertex2DColor(glm::vec2 position = {0, 0}, glm::vec3 color = {0, 0, 0})
+        : _position(position), _color(color) {}
+  };
+
+  struct Square {
+    std::vector<Vertex2DColor> _points;
+
+    Square(Vertex2DColor a, Vertex2DColor b, Vertex2DColor c, Vertex2DColor d) {
+      _points = std::vector<Vertex2DColor>();
+      _points.emplace_back(a);
+      _points.emplace_back(b);
+      _points.emplace_back(c);
+      _points.emplace_back(d);
+    }
+
+    std::vector<Vertex2DColor> points() {
+      return {_points[0], _points[1], _points[2],
+              _points[1], _points[2], _points[3]};
+    }
+
+    std::vector<uint32_t> indices() { return {0, 1, 2, 1, 2, 3}; }
+  };
+
+  struct Triangle {
+    std::vector<Vertex2DColor> _points;
+
+    Triangle(Vertex2DColor a, Vertex2DColor b, Vertex2DColor c) {
+      _points = std::vector<Vertex2DColor>();
+      _points.emplace_back(a);
+      _points.emplace_back(b);
+      _points.emplace_back(c);
+    }
+
+    std::vector<Vertex2DColor> points() {
+      return {_points[0], _points[1], _points[2]};
+    }
+
+    std::vector<uint32_t> indices() { return {0, 1, 2}; }
+  };
+
+  struct Circle {
+    std::vector<Vertex2DColor> _points;
+
+    Circle(Vertex2DColor center, float radius, uint n) {
+      _points = std::vector<Vertex2DColor>();
+      _points.emplace_back(center);
+
+      for (uint i = 0; i < n; i++) {
+        float x = radius * std::cos(2 * i * glm::pi<float>() / n);
+        float y = radius * std::sin(2 * i * glm::pi<float>() / n);
+        std::cout << "(" << x << ", " << y << ")\n";
+        _points.emplace_back(Vertex2DColor(glm::vec2(x, y), center._color));
+      }
+    }
+
+    std::vector<Vertex2DColor> points() {
+      std::vector<Vertex2DColor> tmp = std::vector<Vertex2DColor>();
+      for (uint i = 0; i < _points.size() - 1; i++) {
+        tmp.emplace_back(_points[0]);
+        tmp.emplace_back(_points[i + 1]);
+        std::cout << 0 << ' ' << i + 1;
+        tmp.emplace_back(_points[((i + 1) % (_points.size() - 1)) + 1]);
+        std::cout << " " << (i + 1) % (_points.size() - 1) + 1 << '\n';
+      }
+      std::cout << tmp.size() << '\n';
+      return tmp;
+    }
+
+    std::vector<uint32_t> indices() {
+      std::vector<uint32_t> tmp = std::vector<uint32_t>();
+      for (uint32_t i = 0; i < _points.size() - 1; i++) {
+        tmp.emplace_back(0);
+        tmp.emplace_back(i);
+        std::cout << 0 << ' ' << i + 1;
+        tmp.emplace_back((i + 1) % (_points.size() - 1) + 1);
+        std::cout << " " << (i + 1) % (_points.size() - 1) + 1 << '\n';
+      }
+      std::cout << tmp.size() << '\n';
+      return tmp;
+    }
+  };
+  /*********************************
+   * HERE SHOULD COME THE INITIALIZATION CODE
+   *********************************/
+
+  // Declare your infinite update loop.
+  ctx.update = [&]() {
     /*********************************
-     * HERE SHOULD COME THE INITIALIZATION CODE
+     * HERE SHOULD COME THE RENDERING CODE
      *********************************/
+  };
 
-    // Declare your infinite update loop.
-    ctx.update = [&]() {
-        /*********************************
-         * HERE SHOULD COME THE RENDERING CODE
-         *********************************/
-    };
-
-    // Should be done last. It starts the infinite loop.
-    ctx.start();
+  // Should be done last. It starts the infinite loop.
+  ctx.start();
 }
--- TP6_sphere/main.cpp
+++ TP6_sphere/main.cpp
@@ -2,13 +2,17 @@
 #include "glimac/sphere_vertices.hpp"
 #include "glm/fwd.hpp"
 #include "glm/matrix.hpp"
+#include "img/src/Image.h"
 #include "p6/p6.h"
 #include <cstdlib>
+#include <exception>
 #include <glm/glm.hpp>
 #include <glm/gtc/constants.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/random.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <iostream>
+#include <vector>
 
 int main() {
   auto ctx = p6::Context{{1280, 720, "TP6 Sphère"}};
@@ -20,13 +24,35 @@ int main() {
   const std::vector<glimac::ShapeVertex> vertices =
       glimac::sphere_vertices(1.f, 32, 16);
 
+  img::Image texture = p6::load_image_buffer("assets/textures/EarthMap.jpg");
+  // }
+  // catch (std::exception &e) {
+  //   std::cerr << "Couldn't load the texture\n";
+  //   return EXIT_FAILURE;
+  // }
+
   const p6::Shader shader =
-      p6::load_shader("shaders/3D.vs.glsl", "shaders/normals.fs.glsl");
+      p6::load_shader("shaders/3D.vs.glsl", "shaders/tex3D.fs.glsl");
+
+  // Texture
+  GLuint textures = 0;
+  glGenTextures(1, &textures);
+  glBindTexture(GL_TEXTURE_2D, textures);
+  {
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, texture.width(), texture.height(),
+                 0, GL_RGBA, GL_UNSIGNED_BYTE, (const void *)texture.data());
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  }
+  glBindTexture(GL_TEXTURE_2D, 0);
 
   // Uniform
   GLuint uMVPMatrix = glGetUniformLocation(shader.id(), "uMVPMatrix");
   GLuint uMVMatrix = glGetUniformLocation(shader.id(), "uMVMatrix");
   GLuint uNormalMatrix = glGetUniformLocation(shader.id(), "uNormalMatrix");
+  GLuint uTexture = glGetUniformLocation(shader.id(), "uTexture");
 
   if (uMVPMatrix < 0 || uMVMatrix < 0 || uNormalMatrix < 0) {
     std::cerr << "Uniform not found in shaders. Double check the names.\n";
@@ -34,11 +60,6 @@ int main() {
   }
 
   glEnable(GL_DEPTH_TEST);
-
-  glm::mat4 ProjMatrix =
-      glm::perspective(glm::radians(70.f), ctx.aspect_ratio(), 0.1f, 100.f);
-  glm::mat4 MVMatrix = glm::translate(glm::mat4(1), glm::vec3(0, 0, -5));
-  glm::mat4 NormalMatrix = glm::transpose(glm::inverse(MVMatrix));
 
   // Buffer initialization
   // VBO : Data
@@ -86,6 +107,12 @@ int main() {
   }
   glBindVertexArray(0);
 
+  std::vector<glm::vec3> orbits;
+  orbits.reserve(32);
+  for (int i = 0; i < 32; i++) {
+    orbits[i] = glm::sphericalRand(1.);
+  }
+
   // Declare your infinite update loop.
   ctx.update = [&]() {
     /*********************************
@@ -98,12 +125,37 @@ int main() {
 
     glBindVertexArray(vao);
     {
-      glUniformMatrix4fv(uMVPMatrix, 1, GL_FALSE,
-                         glm::value_ptr(ProjMatrix * MVMatrix));
-      glUniformMatrix4fv(uMVMatrix, 1, GL_FALSE, glm::value_ptr(MVMatrix));
-      glUniformMatrix4fv(uNormalMatrix, 1, GL_FALSE,
-                         glm::value_ptr(NormalMatrix));
-      glDrawArrays(GL_TRIANGLES, 0, vertices.size());
+      glBindTexture(GL_TEXTURE_2D, textures);
+      {
+        glUniform1i(uTexture, 0);
+
+        glm::mat4 ProjMatrix = glm::perspective(
+            glm::radians(70.f), ctx.aspect_ratio(), 0.1f, 100.f);
+        glm::mat4 MVMatrix = glm::translate(glm::mat4(1), glm::vec3(0, 0, -5));
+        glm::mat4 NormalMatrix = glm::transpose(glm::inverse(MVMatrix));
+
+        glUniformMatrix4fv(uMVPMatrix, 1, GL_FALSE,
+                           glm::value_ptr(ProjMatrix * MVMatrix));
+        glUniformMatrix4fv(uMVMatrix, 1, GL_FALSE, glm::value_ptr(MVMatrix));
+        glUniformMatrix4fv(uNormalMatrix, 1, GL_FALSE,
+                           glm::value_ptr(NormalMatrix));
+        glDrawArrays(GL_TRIANGLES, 0, vertices.size());
+
+        for (int i = 0; i < 32; i++) {
+          MVMatrix = glm::translate(glm::mat4{1.f}, {0.f, 0.f, -5.f});
+          MVMatrix = glm::rotate(MVMatrix, ctx.time(), orbits[i]);
+          MVMatrix = glm::translate(MVMatrix, {-2.f, 0.f, 0.f});
+          MVMatrix = glm::scale(MVMatrix, glm::vec3{0.2f});
+
+          glUniformMatrix4fv(uMVPMatrix, 1, GL_FALSE,
+                             glm::value_ptr(ProjMatrix * MVMatrix));
+          glUniformMatrix4fv(uMVMatrix, 1, GL_FALSE, glm::value_ptr(MVMatrix));
+          glUniformMatrix4fv(uNormalMatrix, 1, GL_FALSE,
+                             glm::value_ptr(NormalMatrix));
+          glDrawArrays(GL_TRIANGLES, 0, vertices.size());
+        }
+      }
+      glBindTexture(GL_TEXTURE_2D, 0);
     }
     glBindVertexArray(0);
   };
